9975cabd95af4b0f42e06d9e792698bff41876c7
ikurowski <igorkurowski@gmail.com>
2022-11-24 23:50:28 +0100

    add OutputBox comp

---
+++ src/components/OutputBox.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+import { styles } from '../styles';
+
+// components
+
+export default function OutputBox({ users }) {
+  console.log(users);
+
+  return (
+    <Container>
+      {users.map((user) => (
+        <Output key={Math.random().toString()}>
+         {user.name + ` (${user.age} years old)`}
+        </Output>
+      ))}
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+  background-color: ${styles.color.secondary};
+  margin: 10px;
+  padding: 20px;
+  width: 70%;
+  & :last-child {
+    margin-bottom: 0.5rem;
+  }
+`;
+
+const Output = styled.div`
+  height: 2rem;
+  width: 100%;
+  border-radius: 5px;
+  border: none;
+  padding: 0.5rem;
+  margin-top: 0.5rem;
+  font-size: 1rem;
+  background-color: ${styles.color.textLight};
+`;
