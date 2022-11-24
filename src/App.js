cc544494f5c727dde9f0c44ca9ef9c7a185b22e4
ikurowski <igorkurowski@gmail.com>
2022-11-24 23:48:31 +0100

    add global styles to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Main from './components/Main';
+import GlobalStyle from './globalStyles';
 
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <>
+    <GlobalStyle/>
+    <Main/>
+    </>
   );
 }
 
