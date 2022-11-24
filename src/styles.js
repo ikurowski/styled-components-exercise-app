e3003d2da22ff596644d0358ca204c385793b161
ikurowski <igorkurowski@gmail.com>
2022-11-24 11:44:23 +0100

    add styles config

---
+++ src/styles.js
@@ -0,0 +1,11 @@
+export const styles = {
+  color: {
+    primary: '#001d3d',
+    primaryDark: '#000814',
+    secondary: '#ffd60a',
+    secondaryDark: '#ffc300',
+  },
+  font: {
+    main: 'sans-serif',
+  },
+};
