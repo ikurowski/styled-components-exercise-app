3987d00171e7dff58d889f3f52024334ac3abaec
ikurowski <igorkurowski@gmail.com>
2022-11-24 23:49:44 +0100

    add InputComponent

---
+++ src/components/InputComponent.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function InputComponent({ headerText, type }) {
+  
+  const firstChangeHandler = (e) => {
+    console.log(e.target.value);
+  };
+  return (
+    <Container>
+      <HeaderText>{headerText}</HeaderText>
+      <Input onChange={firstChangeHandler} type={type} />
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  margin: 10px 0;
+  width: 100%;
+`;
+
+const HeaderText = styled.h1`
+  font-size: 1.2rem;
+  margin: 0;
+  margin-bottom: 0.5rem;
+`;
+
+const Input = styled.input`
+  height: 2rem;
+  border-radius: 5px;
+  border: none;
+  padding: 0.5rem;
+  font-size: 1rem;
+  width: 100%;
+  & :focus {
+    outline: none;
+  }
+`;
